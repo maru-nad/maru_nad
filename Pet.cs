f075779cfa510ebb7e0a98872fd2be209b6dedfa
maru-nad <maru24nad11@gmail.com>
2025-10-19 23:59:05 +0300

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Pet
+    {
+        private String name;
+        private int energy;
+        public String nameCheck
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+               name = value;
+            }
+        }
+       
+        public int energyCheck
+        {
+            get
+            {
+                return energy;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    energy = value;
+                }
+                else
+                {
+                    Console.WriteLine("Энергия должна быть от 0 до 100!");
+                    return;
+                    
+                }
+
+
+            }
+        }
+        public void Rest()
+        {
+            energy += 30;
+            Console.WriteLine($"{name} отдыхает, энергия: {energy}");
+        }
+        public void Play()
+        {
+            energy -= 20;
+            Console.WriteLine($"{name} играет, энергия: {energy}");
+        }
+
+
+    }
+}
+
---
+++ Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet = new Pet();
+            pet.nameCheck = "Бобик";
+            pet.energyCheck = 40;
+            pet.Play();
+            pet.Rest();
+        }
+    }
+}
