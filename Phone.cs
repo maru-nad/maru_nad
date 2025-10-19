6f7fe20cb24c33d4a0ae0992598702fa8e8f2475
maru-nad <maru24nad11@gmail.com>
2025-10-19 23:13:20 +0300

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Phone
+    {
+        private String brand;
+        private int batteryLevel;
+        public String brandCheck
+        {
+            get {return brand;}
+            set {brand = value;}
+        }
+        public int batteryLevelCheck
+        {
+            get {return batteryLevel;}
+            set
+            {
+                if (value >= 0 && batteryLevel <= 100)
+                {
+                    batteryLevel = value;
+                }
+                else
+                {
+                    Console.WriteLine("Заряд должен быть от 0 до 100!");
+                }
+            }
+        }
+        public void UsePhone()
+        {
+            batteryLevel -= 10;
+            if (batteryLevel <= 0)
+            {
+                batteryLevel = 0;
+            }
+            Console.WriteLine($"Телефон {brand}, заряд:{batteryLevel}%");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone();
+            phone.brandCheck ="Samsung";
+            phone.batteryLevelCheck =0;
+            phone.UsePhone();
+            phone.UsePhone();
+        }
+    }
+}
