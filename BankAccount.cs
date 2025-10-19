14738d2023d94c5e5a8ff4d62780bd14c4af634d
maru-nad <maru24nad11@gmail.com>
2025-10-19 23:32:22 +0300

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class BankAccount
+    {
+        private String owner;
+        private double balance;
+        public String ownerCheck
+        {
+            get { return owner; }
+            set
+            {
+                owner = value;
+            }
+        }
+        public double balanceCheck
+        {
+            get { return balance; }
+            set
+            {
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Баланс не должен быть меньше 100.");
+                    return;
+                }
+
+            }
+        }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"{owner}, баланс: {balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Сумма депозита должна быть больше 0.");
+                return ;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"{owner}, баланс: {balance}.");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Недостаточно средств на счете.");
+                    return ;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Сумма снятия должна быть больше 0.");
+                return;
+            }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.ownerCheck = "Иван";
+            account.balanceCheck = 1000;
+
+            account.Deposit(50.0);
+            account.Withdraw(20.0);
+            account.Withdraw(15000); 
+        }
+    
+    }
+}
