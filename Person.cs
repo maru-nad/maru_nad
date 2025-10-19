3c5b24947b99b0f5a40803ad1d6a086202143012
maru-nad <maru24nad11@gmail.com>
2025-10-19 22:51:47 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Person
+    {
+        private String name;
+           private int age; 
+        public String nameCheck
+        {
+            get { return name; }
+            set
+            {
+              name = value;
+            }
+        }
+        public int ageCheck
+        {
+            get { return age; }
+            set
+            {
+                if (value>=0&&value<=120)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Возраст должен быть в диапазоне от 0 до 120.");
+                    return;
+                }
+
+            }
+        }
+        public void SayHello()
+        {
+            Console.WriteLine($"Привет, я {name}, мне {age} лет!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.nameCheck = "Маша";
+            person.ageCheck = 10;
+            person.SayHello();
+        }
+    }
+}
