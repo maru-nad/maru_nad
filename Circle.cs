565f7385a36ee8a8188578700c1455c356a86b68
maru-nad <maru24nad11@gmail.com>
2025-10-19 23:42:02 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Circle
+    {
+        private double radius;
+        public double radiusCheck
+        {
+            get { return radius; }
+            set
+            {
+                if (value >= 0)
+                {
+                    radius = value;
+                }
+                else
+                {
+                    Console.WriteLine("Радиус должен быть больше 0.");
+                    return;
+                }
+
+            }
+        }
+        public void GetArea()
+        {
+            double S;
+            S= Math.PI * radius*radius;
+            Console.WriteLine($"Площадь:{S}");
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.radiusCheck = 7;
+            circle.GetArea();
+        }
+    }
+}
